825fed10ac1b70b2dd77333bc9e202c765cc2909
Chhavi Jain <95961418+chhavijain221864@users.noreply.github.com>
2023-05-31 14:51:25 +0530

    Add files via upload

---
+++ newproject/enroll/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+# Create your models here.
+class User(models.Model):
+    Name=models.CharField(max_length=50)
+    Email=models.EmailField(max_length=50)
+    Password=models.CharField(max_length=50)
